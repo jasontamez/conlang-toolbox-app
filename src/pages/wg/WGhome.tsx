844cb4fe068b32bcf5d5a300b72ae5c04a127855
Jason Tamez <druidic@gmail.com>
2021-01-29 01:14:06 -0600

    Beginning the WG/Home page

--- src/pages/wg/WGhome.tsx
+++ src/pages/wg/WGhome.tsx
@@ -8,7 +8,7 @@ import {
 	IonButtons,
 	IonTitle
 } from '@ionic/react';
-import ExploreContainer from '../../components/ExploreContainer';
+import I from '../../components/IPA';
 import '../WordGen.css';
 
 const WGHome = () => {
@@ -23,7 +23,19 @@ const WGHome = () => {
 				</IonToolbar>
 			</IonHeader>
 			<IonContent fullscreen>
-				<ExploreContainer name="Home page" />
+				<div>
+					<p>
+						This is where you define categories of sounds. The two simplest categories
+						are <em>consonants</em> and <em>vowels</em>, but you may want to create multiple
+						categories depending on how you want your language's syllables formed. For example,
+						the consonants <I>pbk</I> in English may be followed by the consonants <I>lr</I> at
+						the beginning of syllables. So you might choose them as categories, while also
+						putting <I>pbklr</I> in a third category for general consonants.
+					</p><p>
+						These <strong>Categories</strong> of sounds will be used in
+						the <strong>Syllables</strong> tab to generate your words.
+					</p>
+				</div>
 			</IonContent>
 		</IonPage>
 	);
