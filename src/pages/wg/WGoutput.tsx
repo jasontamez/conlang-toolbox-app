1816128ade803f98845cd7ba3108c49015cbd4de
Jason Tamez <druidic@gmail.com>
2021-02-15 02:36:42 -0600

    Spun off category matching to its own module

--- src/pages/wg/WGoutput.tsx
+++ src/pages/wg/WGoutput.tsx
@@ -36,11 +36,11 @@ import {
 	bookOutline,
 	saveOutline
 } from 'ionicons/icons';
-import escapeRegexp from 'escape-string-regexp';
 import OutputOptionsModal from './M-OutputOptions';
 import debounce from '../../components/Debounce';
 import { v4 as uuidv4 } from 'uuid';
 import { $a } from '../../components/DollarSignExports';
+import calculateCategoryReferenceRegex from '../../components/CategoryRegex';
 import '../App.css';
 
 const WGOut = () => {
@@ -85,58 +85,6 @@ const WGOut = () => {
 		return Math.ceil(max).toString() + "px";
 	};
 
-	const calculateCategoryReferenceRegex = (rule: WGRewriteRuleObject) => {
-		// Check rewrite rules for %Category references
-		// %% condenses to %, so split on those to begin with.
-		let broken = rule.seek.split("%%");
-		// Create a variable to hold the pieces as they are handled
-		let final = [];
-		while(broken.length > 0) {
-			// First, check for category negation
-			// Separate along !% instances
-			let testing = broken.shift()!.split("!%");
-			// Save first bit, before any !%
-			let reformed = testing.shift();
-			// Handle each instance
-			while(testing.length > 0) {
-				let bit = testing.shift();
-				// What's the category being negated?
-				let category = catMap.get(bit!.charAt(0));
-				// Does it exist?
-				if(category !== undefined) {
-					// Category found. Replace with [^a-z] construct, where a-z is the category contents.
-					reformed += "[^" + escapeRegexp(category.run) + "]";
-					// If category is not found, it gets ignored.
-				}
-				// Remove category identifier, save to reformed
-				reformed += bit!.slice(1);
-			}
-			// Now check for categories
-			// Separate along % instances
-			testing = reformed!.split("%");
-			// Save the first bit, before any %
-			reformed = testing.shift();
-			// Handle each instance
-			while(testing.length > 0) {
-				let bit = testing.shift();
-				// What's the category?
-				let category = catMap.get(bit!.charAt(0));
-				// Does it exist?
-				if(category !== undefined) {
-					// Category found. Replace with [a-z] construct, where a-z is the category contents.
-					reformed += "[" + escapeRegexp(category.run) + "]";
-					// If category is not found, it gets ignored.
-				}
-				// Remove category identifier, save to reformed
-				reformed += bit!.slice(1);
-			}
-			// Save reformed for later!
-			final.push(reformed);
-		}
-		// Reform info with %% reduced back to % and save as regexp
-		return new RegExp(final.join("%"), "g");
-	};
-
 	const generateOutput = (output: HTMLElement) => {
 		let text: HTMLElement[] = [];
 		let type = settingsWG.output;
@@ -173,7 +121,7 @@ const WGOut = () => {
 			let regex: RegExp;
 			if(rule.seek.indexOf("%") !== -1) {
 				// Found a possibility.
-				regex = calculateCategoryReferenceRegex(rule);
+				regex = calculateCategoryReferenceRegex(rule.seek, catMap) as RegExp;
 			} else {
 				regex = new RegExp(rule.seek, "g");
 			}
