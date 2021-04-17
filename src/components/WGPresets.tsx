5a03a0219eb10871d8491688f46f545488b2febd
Jason Tamez <jasontamez@users.noreply.github.com>
2021-04-16 22:46:31 -0500

    *bugfix: reorganize Complex transforms

--- src/components/WGPresets.tsx
+++ src/components/WGPresets.tsx
@@ -616,288 +616,6 @@ const WGPresets: WGPresetObject = new Map([
 		wordgenRewriteRules: {
 			list: [
 				{
-					key: "0",
-					seek: "([aeiou])\\1{2,}",
-					replace: "$1$1",
-					description: "change triple-vowels to double vowels"
-				},
-				{
-					key: "1",
-					seek: "([AEOU])\\1+",
-					replace: "$1",
-					description: "change double-dipthongs to single"
-				},
-				{
-					key: "2",
-					seek: "(%V{2})%V+",
-					replace: "$1",
-					description: "eliminate third vowel in a row"
-				},
-				{
-					key: "3",
-					seek: "h+",
-					replace: "h",
-					description: "reduce multiple h to single"
-				},
-				{
-					key: "4",
-					seek: "h(?=%V(%E|%C{0,2}%V)\\b)",
-					replace: "H",
-					description: "save h before stressed syllable"
-				},
-				{
-					key: "5",
-					seek: "(%V)h(?=%V\\b)",
-					replace: "$1H",
-					description: "save h before stressed syllable"
-				},
-				{
-					key: "6",
-					seek: "\\bh",
-					replace: "H",
-					description: "save word-initial h"
-				},
-				{
-					key: "7",
-					seek: "h\\b",
-					replace: "H",
-					description: "save word-final h"
-				},
-				{
-					key: "8",
-					seek: "h",
-					replace: "",
-					description: "eliminate all other h"
-				},
-				{
-					key: "9",
-					seek: "H",
-					replace: "h",
-					description: "restore saved h"
-				},
-				{
-					key: "9.1",
-					seek: "kh",
-					replace: "k",
-					description: "reduce kh to k"
-				},
-				{
-					key: "10",
-					seek: "A",
-					replace: "aĭ",
-					description: "dipthong"
-				},
-				{
-					key: "11",
-					seek: "O",
-					replace: "oĭ",
-					description: "dipthong"
-				},
-				{
-					key: "12",
-					seek: "U",
-					replace: "uĭ",
-					description: "dipthong"
-				},
-				{
-					key: "13",
-					seek: "E",
-					replace: "eĭ",
-					description: "dipthong"
-				},
-				{
-					key: "14",
-					seek: "ĭi",
-					replace: "i",
-					description: "eliminate dipthong before i"
-				},
-				{
-					key: "15",
-					seek: "ĭT",
-					replace: "ĭt",
-					description: "de-retroflex t after a dipthong"
-				},
-				{
-					key: "16",
-					seek: "ĭS",
-					replace: "ĭs",
-					description: "de-retroflex s after a dipthong"
-				},
-				{
-					key: "17",
-					seek: "ĭL",
-					replace: "ĭl",
-					description: "de-retroflex l after a dipthong"
-				},
-				{
-					key: "18",
-					seek: "ĭN",
-					replace: "ĭn",
-					description: "de-retroflex n after a dipthong"
-				},
-				{
-					key: "19",
-					seek: "(.\\B[aeou])i",
-					replace: "$1ĭ",
-					description: "change certain non-word-initial vowel-i pairs to dipthongs"
-				},
-				{
-					key: "20",
-					seek: "(%C)\\1",
-					replace: "$1",
-					description: "reduce double consonants to one"
-				},
-				{
-					key: "21",
-					seek: "[tkpT]r",
-					replace: "r",
-					description: "remove plosive before an r"
-				},
-				{
-					key: "22",
-					seek: "n[pTk]",
-					replace: "nt",
-					description: "change n-plosive to nt"
-				},
-				{
-					key: "23",
-					seek: "m[tTk]",
-					replace: "mp",
-					description: "change m-plosive to mp"
-				},
-				{
-					key: "24",
-					seek: "N[ptk]",
-					replace: "NT",
-					description: "retroflex plosive after retroflex n"
-				},
-				{
-					key: "25",
-					seek: "k[nmN]",
-					replace: "k",
-					description: "remove nasal after k"
-				},
-				{
-					key: "26",
-					seek: "p[nN]",
-					replace: "pm",
-					description: "change p-nasal to pm"
-				},
-				{
-					key: "27",
-					seek: "t[mN]",
-					replace: "tn",
-					description: "change t-nasal to tn"
-				},
-				{
-					key: "28",
-					seek: "T[nm]",
-					replace: "TN",
-					description: "make post-retroflex t nasal retroflex n"
-				},
-				{
-					key: "29",
-					seek: "p[sSh]",
-					replace: "pf",
-					description: "change p-fricative to pf"
-				},
-				{
-					key: "30",
-					seek: "t[fSh]",
-					replace: "ts",
-					description: "change t-fricative to ts"
-				},
-				{
-					key: "31",
-					seek: "T[fsh]",
-					replace: "TS",
-					description: "change post-retroflex t fricative to retroflex s"
-				},
-				{
-					key: "32",
-					seek: "k[fsS]",
-					replace: "kh",
-					description: "change k-fricative to kh"
-				},
-				{
-					key: "33",
-					seek: "f[sSh]",
-					replace: "fp",
-					description: "change f-fricative to fp"
-				},
-				{
-					key: "34",
-					seek: "s[fSh]",
-					replace: "st",
-					description: "change s-fricative to st"
-				},
-				{
-					key: "35",
-					seek: "S[fsh]",
-					replace: "ST",
-					description: "change post-retroflex s fricative to retroflex t"
-				},
-				{
-					key: "36",
-					seek: "h[fsS]",
-					replace: "hk",
-					description: "change h-fricative to hk"
-				},
-				{
-					key: "37",
-					seek: "ft",
-					replace: "fp",
-					description: "change ft to fp"
-				},
-				{
-					key: "38",
-					seek: "sT",
-					replace: "st",
-					description: "de-retroflex t after s"
-				},
-				{
-					key: "39",
-					seek: "St",
-					replace: "ST",
-					description: "make t after retroflex s into retroflex t"
-				},
-				{
-					key: "40",
-					seek: "([TSLN])[tsln]",
-					replace: "$1",
-					description: "eliminate non-retroflex consonant after retroflex consonants"
-				},
-				{
-					key: "41",
-					seek: "([tsln])[TSLN]",
-					replace: "$1",
-					description: "eliminate retroflex consonant after non-retroflex consonant"
-				},
-				{
-					key: "42",
-					seek: "NT",
-					replace: "nT",
-					description: "de-retroflex n before retroflex t"
-				},
-				{
-					key: "43",
-					seek: "TN",
-					replace: "tN",
-					description: "de-retroflex t before retroflex n"
-				},
-				{
-					key: "44",
-					seek: "ST",
-					replace: "sT",
-					description: "de-retroflex s before retroflex t"
-				},
-				{
-					key: "45",
-					seek: "TS",
-					replace: "tS",
-					description: "de-retroflex t before retroflex s"
-				},
-				{
 					key: "50.1",
 					seek: "1",
 					replace: "e",
@@ -1232,6 +950,288 @@ const WGPresets: WGPresetObject = new Map([
 					seek: "([nstNST])~",
 					replace: "$1oĭa",
 					description: "conjugation: GER"
+				},
+				{
+					key: "0",
+					seek: "([aeiou])\\1{2,}",
+					replace: "$1$1",
+					description: "change triple-vowels to double vowels"
+				},
+				{
+					key: "1",
+					seek: "([AEOU])\\1+",
+					replace: "$1",
+					description: "change double-dipthongs to single"
+				},
+				{
+					key: "2",
+					seek: "(%V{2})%V+",
+					replace: "$1",
+					description: "eliminate third vowel in a row"
+				},
+				{
+					key: "3",
+					seek: "h+",
+					replace: "h",
+					description: "reduce multiple h to single"
+				},
+				{
+					key: "4",
+					seek: "h(?=%V(%E|%C{0,2}%V)\\b)",
+					replace: "H",
+					description: "save h before stressed syllable"
+				},
+				{
+					key: "5",
+					seek: "(%V)h(?=%V\\b)",
+					replace: "$1H",
+					description: "save h before stressed syllable"
+				},
+				{
+					key: "6",
+					seek: "\\bh",
+					replace: "H",
+					description: "save word-initial h"
+				},
+				{
+					key: "7",
+					seek: "h\\b",
+					replace: "H",
+					description: "save word-final h"
+				},
+				{
+					key: "8",
+					seek: "h",
+					replace: "",
+					description: "eliminate all other h"
+				},
+				{
+					key: "9",
+					seek: "H",
+					replace: "h",
+					description: "restore saved h"
+				},
+				{
+					key: "9.1",
+					seek: "kh",
+					replace: "k",
+					description: "reduce kh to k"
+				},
+				{
+					key: "10",
+					seek: "A",
+					replace: "aĭ",
+					description: "dipthong"
+				},
+				{
+					key: "11",
+					seek: "O",
+					replace: "oĭ",
+					description: "dipthong"
+				},
+				{
+					key: "12",
+					seek: "U",
+					replace: "uĭ",
+					description: "dipthong"
+				},
+				{
+					key: "13",
+					seek: "E",
+					replace: "eĭ",
+					description: "dipthong"
+				},
+				{
+					key: "14",
+					seek: "ĭi",
+					replace: "i",
+					description: "eliminate dipthong before i"
+				},
+				{
+					key: "15",
+					seek: "ĭT",
+					replace: "ĭt",
+					description: "de-retroflex t after a dipthong"
+				},
+				{
+					key: "16",
+					seek: "ĭS",
+					replace: "ĭs",
+					description: "de-retroflex s after a dipthong"
+				},
+				{
+					key: "17",
+					seek: "ĭL",
+					replace: "ĭl",
+					description: "de-retroflex l after a dipthong"
+				},
+				{
+					key: "18",
+					seek: "ĭN",
+					replace: "ĭn",
+					description: "de-retroflex n after a dipthong"
+				},
+				{
+					key: "19",
+					seek: "(.\\B[aeou])i",
+					replace: "$1ĭ",
+					description: "change certain non-word-initial vowel-i pairs to dipthongs"
+				},
+				{
+					key: "20",
+					seek: "(%C)\\1",
+					replace: "$1",
+					description: "reduce double consonants to one"
+				},
+				{
+					key: "21",
+					seek: "[tkpT]r",
+					replace: "r",
+					description: "remove plosive before an r"
+				},
+				{
+					key: "22",
+					seek: "n[pTk]",
+					replace: "nt",
+					description: "change n-plosive to nt"
+				},
+				{
+					key: "23",
+					seek: "m[tTk]",
+					replace: "mp",
+					description: "change m-plosive to mp"
+				},
+				{
+					key: "24",
+					seek: "N[ptk]",
+					replace: "NT",
+					description: "retroflex plosive after retroflex n"
+				},
+				{
+					key: "25",
+					seek: "k[nmN]",
+					replace: "k",
+					description: "remove nasal after k"
+				},
+				{
+					key: "26",
+					seek: "p[nN]",
+					replace: "pm",
+					description: "change p-nasal to pm"
+				},
+				{
+					key: "27",
+					seek: "t[mN]",
+					replace: "tn",
+					description: "change t-nasal to tn"
+				},
+				{
+					key: "28",
+					seek: "T[nm]",
+					replace: "TN",
+					description: "make post-retroflex t nasal retroflex n"
+				},
+				{
+					key: "29",
+					seek: "p[sSh]",
+					replace: "pf",
+					description: "change p-fricative to pf"
+				},
+				{
+					key: "30",
+					seek: "t[fSh]",
+					replace: "ts",
+					description: "change t-fricative to ts"
+				},
+				{
+					key: "31",
+					seek: "T[fsh]",
+					replace: "TS",
+					description: "change post-retroflex t fricative to retroflex s"
+				},
+				{
+					key: "32",
+					seek: "k[fsS]",
+					replace: "kh",
+					description: "change k-fricative to kh"
+				},
+				{
+					key: "33",
+					seek: "f[sSh]",
+					replace: "fp",
+					description: "change f-fricative to fp"
+				},
+				{
+					key: "34",
+					seek: "s[fSh]",
+					replace: "st",
+					description: "change s-fricative to st"
+				},
+				{
+					key: "35",
+					seek: "S[fsh]",
+					replace: "ST",
+					description: "change post-retroflex s fricative to retroflex t"
+				},
+				{
+					key: "36",
+					seek: "h[fsS]",
+					replace: "hk",
+					description: "change h-fricative to hk"
+				},
+				{
+					key: "37",
+					seek: "ft",
+					replace: "fp",
+					description: "change ft to fp"
+				},
+				{
+					key: "38",
+					seek: "sT",
+					replace: "st",
+					description: "de-retroflex t after s"
+				},
+				{
+					key: "39",
+					seek: "St",
+					replace: "ST",
+					description: "make t after retroflex s into retroflex t"
+				},
+				{
+					key: "40",
+					seek: "([TSLN])[tsln]",
+					replace: "$1",
+					description: "eliminate non-retroflex consonant after retroflex consonants"
+				},
+				{
+					key: "41",
+					seek: "([tsln])[TSLN]",
+					replace: "$1",
+					description: "eliminate retroflex consonant after non-retroflex consonant"
+				},
+				{
+					key: "42",
+					seek: "NT",
+					replace: "nT",
+					description: "de-retroflex n before retroflex t"
+				},
+				{
+					key: "43",
+					seek: "TN",
+					replace: "tN",
+					description: "de-retroflex t before retroflex n"
+				},
+				{
+					key: "44",
+					seek: "ST",
+					replace: "sT",
+					description: "de-retroflex s before retroflex t"
+				},
+				{
+					key: "45",
+					seek: "TS",
+					replace: "tS",
+					description: "de-retroflex t before retroflex s"
 				},
 				{
 					key: "46",
